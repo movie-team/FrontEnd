8ec197270ba7a9c15e73e4ca13eba53fe41cb49f
calendar2 <zoro94317@gmail.com>
2023-11-10 17:53:03 +0900

    [231110] 차민재

--- movie-front/src/App.vue
+++ movie-front/src/App.vue
@@ -4,10 +4,11 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <!-- <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav> -->
+    <nav>
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+      <RouterLink :to="{ name: 'signup' }">회원가입</RouterLink>
+      <RouterLink :to="{ name: 'login' }">로그인</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
--- movie-front/src/router/index.js
+++ movie-front/src/router/index.js
@@ -1,13 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import SignUp from '@/views/SignUp.vue'
+import Login from '@/views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
     // {
     //   path: '/about',
     //   name: 'about',
